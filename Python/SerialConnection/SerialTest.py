0dd0d046b24917f99c89559d393e6a836b048a0f
KlasMagnus <magnusjohansson82@gmail.com>
2015-05-27 11:08:45 +0200

    added Python serial test program
    configured for windows, opens serial connection on COM4 and prints the
    lines received from the arduino

---
+++ Python/SerialConnection/SerialTest.py
@@ -0,0 +1,14 @@
+__author__ = 'Magnus Johansson'
+#opens serial connection on COM4 and prints received lines
+import serial
+import time
+ser = serial.Serial('\\.\COM4', 9600, timeout=0)
+while 1:
+    try:
+        line = ser.readline()
+        print(line)
+        time.sleep(1)
+    except ser.SerialTimeoutException:
+        print('Data could not be read')
+        time.sleep(1)
+
