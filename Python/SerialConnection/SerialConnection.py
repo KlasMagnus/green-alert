94b9713debff4003f0a1dba067c16d252a687d15
KlasMagnus <magnusjohansson82@gmail.com>
2015-05-28 15:30:02 +0200

    simplified serial connection
    removed unnecessary check, web interface will check for error instead

--- Python/SerialConnection/SerialConnection.py
+++ Python/SerialConnection/SerialConnection.py
@@ -1,5 +1,5 @@
 __author__ = 'Magnus Johansson'
-#opens serial connection and writes incoming temperature value(or error message) to a file
+#opens serial connection and writes incoming temperature values(or error msg) to a file
 import serial
 import time
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0)
@@ -7,13 +7,7 @@ while 1:
     with open('workfile', 'w') as f:
                 try:
                         line = ser.readline()
-                        if line == "OK":
-                                line = ser.readline()
-                                tempstr = 'Current temperature in Celsius :'
-                                tempstr += line
-                                f.write(tempstr)
-                        else:
-                                f.write(line);
+                        f.write(line)
                 except ser.SerialTimeoutException:
                         f.write('Data could not be read')
     f.close()
